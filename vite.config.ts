324e7cc2c408a381feaa161165236dc5b672d48f
typing-hanwei <hanwei@typing.email>
2024-10-24 16:57:26 +0800

    feat: 增加删除页面

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { globbySync } from 'globby';
-let homeHTML = globbySync('./home/*.html');
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: ['index.html', 'facility.html', 'practice.html']
+      input: ['index.html', 'facility.html', 'practice.html', 'delete.html']
     },
     minify: true,
     sourcemap: false

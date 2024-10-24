9f39768d2941a0db572504c947ea8961cf2aa675
typing-hanwei <hanwei@typing.email>
2024-10-24 11:33:32 +0800

    feat: 样式调整

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { globbySync } from 'globby'
-let homeHTML = globbySync('./home/*.html')
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { globbySync } from 'globby';
+let homeHTML = globbySync('./home/*.html');
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: ['index.html', ...homeHTML],
+      input: ['index.html', 'facility.html', 'practice.html']
     },
     minify: true,
     sourcemap: false
@@ -14,10 +14,10 @@ export default defineConfig({
   esbuild: {
     drop: ['console', 'debugger'],
     target: 'es2015',
-    legalComments: 'none',
+    legalComments: 'none'
   },
   plugins: [vue()],
   server: {
-    port: 5100,
-  },
-})
+    port: 5100
+  }
+});
